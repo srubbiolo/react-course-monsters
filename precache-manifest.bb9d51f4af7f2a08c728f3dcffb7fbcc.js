31908ec0f5493d96354f4f6b9b02a7dedc572b06
Santiago Rubbiolo <Santiago.rubbiolo@sprint.com>
2020-04-26 18:20:53 -0300

    Updates

---
+++ precache-manifest.bb9d51f4af7f2a08c728f3dcffb7fbcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e8e9c02510cb7335b62faadc6959d6",
+    "url": "/react-course-monsters/index.html"
+  },
+  {
+    "revision": "f817c92705da466f8271",
+    "url": "/react-course-monsters/static/css/main.07167b9b.chunk.css"
+  },
+  {
+    "revision": "d8bd49f9f56d4d850a0d",
+    "url": "/react-course-monsters/static/js/2.43b7047b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-course-monsters/static/js/2.43b7047b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f817c92705da466f8271",
+    "url": "/react-course-monsters/static/js/main.2d28b6d2.chunk.js"
+  },
+  {
+    "revision": "f463bff51b4c09a8b4f0",
+    "url": "/react-course-monsters/static/js/runtime-main.b00d8d97.js"
+  }
+]);
